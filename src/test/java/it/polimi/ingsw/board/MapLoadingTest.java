22267630792b58607b2075fd6d3d13e1b3e661cb
Theodore Tsai <yuting.cai@mail.polimi.it>
2019-04-15 01:42:49 +0200

    Added assertion for no one way doors to all maps and positively verified all of them.

--- src/test/java/it/polimi/ingsw/board/MapLoadingTest.java
+++ src/test/java/it/polimi/ingsw/board/MapLoadingTest.java
@@ -31,13 +31,86 @@ public class MapLoadingTest {
 
     @Test
 
-    /*Test asserts there are no one way doors, if a square has another square as north that square must have the
-    first square as south, does not test a method, only verifies that the files have been written correctly.
+    /**Test asserts there are no one way doors, if a square has another square as north that square must have the
+    *first square as south, does not test a method, only verifies that the files have been written correctly.
+     *
+     * Done for each map.
+     * */
 
-     */
-    public void testNoOneWayDoors(){
+    public void testNoOneWayDoorsLarge() {
 
         String selection = "large";
+
+        Square[][] squareMatrix = new Square[3][4];
+
+        loadMap(selection, squareMatrix);
+
+        int row;
+        int column;
+
+        for (row = 0; row < 3; row++) {
+            for (column = 0; column < 4; column++) {
+
+                if (row != 0 && squareMatrix[row][column].getNorth() == squareMatrix[row - 1][column]) {
+                    assertEquals(squareMatrix[row - 1][column].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (column != 3 && squareMatrix[row][column].getEast() == squareMatrix[row][column + 1]) {
+                    assertEquals(squareMatrix[row][column + 1].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (row != 2 && squareMatrix[row][column].getSouth() == squareMatrix[row + 1][column]) {
+                    assertEquals(squareMatrix[row + 1][column].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (column != 0 && squareMatrix[row][column].getWest() == squareMatrix[row][column - 1]) {
+                    assertEquals(squareMatrix[row][column - 1].getSouth(), squareMatrix[row][column]);
+                }
+
+            }
+        }
+    }
+
+    @Test
+    public void testNoOneWayDoorsMedium() {
+
+        String selection = "medium";
+
+        Square[][] squareMatrix = new Square[3][4];
+
+        loadMap(selection, squareMatrix);
+
+        int row;
+        int column;
+
+        for (row = 0; row < 3; row++) {
+            for (column = 0; column < 4; column++) {
+
+                if (row != 0 && squareMatrix[row][column].getNorth() == squareMatrix[row - 1][column]) {
+                    assertEquals(squareMatrix[row - 1][column].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (column != 3 && squareMatrix[row][column].getEast() == squareMatrix[row][column + 1]) {
+                    assertEquals(squareMatrix[row][column + 1].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (row != 2 && squareMatrix[row][column].getSouth() == squareMatrix[row + 1][column]) {
+                    assertEquals(squareMatrix[row + 1][column].getSouth(), squareMatrix[row][column]);
+                }
+
+                if (column != 0 && squareMatrix[row][column].getWest() == squareMatrix[row][column - 1]) {
+                    assertEquals(squareMatrix[row][column - 1].getSouth(), squareMatrix[row][column]);
+                }
+
+            }
+        }
+    }
+
+
+    @Test
+    public void testNoOneWayDoorsSmall(){
+
+        String selection = "small";
 
         Square[][] squareMatrix = new Square[3][4];
 
@@ -53,8 +126,22 @@ public class MapLoadingTest {
                     assertEquals(squareMatrix[row-1][column].getSouth(),squareMatrix[row][column]);
                 }
 
+                if(column != 3 && squareMatrix[row][column].getEast() == squareMatrix[row][column+1]){
+                    assertEquals(squareMatrix[row][column+1].getSouth(),squareMatrix[row][column]);
+                }
+
+                if(row != 2 && squareMatrix[row][column].getSouth() == squareMatrix[row+1][column]){
+                    assertEquals(squareMatrix[row+1][column].getSouth(),squareMatrix[row][column]);
+                }
+
+                if(column != 0 && squareMatrix[row][column].getWest() == squareMatrix[row][column-1]){
+                    assertEquals(squareMatrix[row][column-1].getSouth(),squareMatrix[row][column]);
+                }
+
             }
         }
+
+
 
 
     }
