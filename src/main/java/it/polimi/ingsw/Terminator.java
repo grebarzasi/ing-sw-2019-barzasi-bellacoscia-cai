b97c5f2c3f4bc14c0521a1cbd283a89b7249be31
theodoretsai <yuting.cai@mail.polimi.it>
2019-05-02 21:54:16 +0200

    Added Terminator skeleton

--- src/main/java/it/polimi/ingsw/Terminator.java
+++ src/main/java/it/polimi/ingsw/Terminator.java
@@ -1,24 +1,55 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.actions.Action;
+import it.polimi.ingsw.actions.Move;
+import it.polimi.ingsw.actions.Shoot;
 import it.polimi.ingsw.board.Square;
 
-public class Terminator {
+public class Terminator extends Figure{
 
-    private Figure owner;
-    private boolean move;
+    private Player owner;
 
-    public Terminator(){
+    private Move move;
+    private Shoot shot;
 
+    public Terminator(String character, Player owner) {
+        super(character);
+        this.owner = owner;
     }
 
     public void pass(){
 
     }
 
-    public void doDamage(Figure p){
+    public void doDamage(Player p){
 
     }
     public void doMove(Square s){
 
+    }
+
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    public void setMove(Move move) {
+        this.move = move;
+    }
+
+    public Shoot getShoot() {
+        return shot;
+    }
+
+    public void setShoot(Shoot shoot) {
+        this.shot = shoot;
     }
 }
