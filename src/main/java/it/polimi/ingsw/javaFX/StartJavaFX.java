c7afc01e7929b631f39babbad3b185111069d5a1
Carlo Bellacoscia <carlo.bellacoscia@mail.polimi.it>
2019-05-22 11:57:14 +0200

    Update JavaFX

--- src/main/java/it/polimi/ingsw/javaFX/StartJavaFX.java
+++ src/main/java/it/polimi/ingsw/javaFX/StartJavaFX.java
@@ -9,6 +9,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -17,6 +18,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  *
@@ -45,30 +48,75 @@ public class StartJavaFX extends Application {
 
         Group root = new Group();
         Scene theScene = new Scene(root);
-        theScene.setFill(Color.BLACK);
         primaryStage.setScene(theScene);
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(50);
         grid.setVgap(30);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setPadding(new Insets(25, 25, 0, 25));
 
         Scene scene = new Scene(grid,1045,720);
         primaryStage.setScene(scene);
-        scene.setFill(Color.BLACK);
 
-        Image back = new Image(new FileInputStream("src/main/resources/images/launcher.jpg"),1080,720,true,true);
+        /**
+         * set background
+         */
+        Image back = new Image(new FileInputStream("src/main/resources/images/black.png"),3000,3000,true,true);
         BackgroundImage backgroundImage = new BackgroundImage(back, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
         grid.setBackground(new Background(backgroundImage));
 
+        /**
+         * set title
+         */
+        Image title;
+        ImageView titleV;
+        title = new Image(new FileInputStream("src/main/resources/images/title.png"),500,200,true,true);
+        titleV = new ImageView(title);
+        VBox ver = new VBox();
+        ver.setAlignment(Pos.TOP_CENTER);
+        ver.getChildren().add(titleV);
+
+        /**
+         * set bottom characters
+         */
+        ImageView yellowV = new ImageView(new Image(new FileInputStream("src/main/resources/images/D-struct-0R.png"), 200 ,200,true,true));
+        ImageView redV = new ImageView(new Image(new FileInputStream("src/main/resources/images/violet.png"), 200 ,200,true,true));;
+        ImageView blueV = new ImageView(new Image(new FileInputStream("src/main/resources/images/banshee.png"), 200 ,200,true,true));;
+        ImageView greenV = new ImageView(new Image(new FileInputStream("src/main/resources/images/sprog.png"), 200 ,200,true,true));;
+        ImageView grayV = new ImageView(new Image(new FileInputStream("src/main/resources/images/dozer.png"), 200 ,200,true,true));;
+
+        HBox charList = new HBox(50);
+        charList.setAlignment(Pos.BOTTOM_CENTER);
+        charList.getChildren().add(yellowV);
+        charList.getChildren().add(redV);
+        charList.getChildren().add(blueV);
+        charList.getChildren().add(greenV);
+        charList.getChildren().add(grayV);
+
+
+        /**
+         * set buttons
+         */
         Button btnRules = new Button("Rules");
         Button btnLogin = new Button("Login");
         Button btnSettings = new Button("Settings");
 
-        grid.add(btnRules,1,6);
-        grid.add(btnLogin,2,6);
-        grid.add(btnSettings,3,6);
+        HBox buttonList = new HBox(20);
+        buttonList.setAlignment(Pos.CENTER);
+        buttonList.getChildren().add(btnRules);
+        buttonList.getChildren().add(btnLogin);
+        buttonList.getChildren().add(btnSettings);
+
+        /**
+         * merge layout
+         */
+        VBox layout = new VBox(200);
+        layout.getChildren().add(ver);
+        layout.getChildren().add(buttonList);
+        layout.getChildren().add(charList);
+
+        grid.add(layout,1,1);
 
 
         btnLogin.setOnAction(e -> {
@@ -94,7 +142,7 @@ public class StartJavaFX extends Application {
         });
 
 
-
+        primaryStage.setMaximized(true);
         primaryStage.show();
 
     }
@@ -102,9 +150,11 @@ public class StartJavaFX extends Application {
     public class loginWindow extends Stage{
         public loginWindow(){
 
-            Scene scene = new Scene(new BorderPane(),200,100);
+            Scene scene = new Scene(new BorderPane(),700,400);
             this.setTitle("Login");
             this.setScene(scene);
+            scene.setFill(Color.BLACK);
+
 
             GridPane loginGrid = new GridPane();
             loginGrid.setAlignment(Pos.CENTER);
@@ -112,7 +162,7 @@ public class StartJavaFX extends Application {
             loginGrid.setVgap(10);
             loginGrid.setPadding(new Insets(25, 25, 25, 25));
 
-            Scene loginScene = new Scene(loginGrid, 300, 275);
+            Scene loginScene = new Scene(loginGrid, 700, 400);
             this.setScene(loginScene);
 
             Text scenetitle = new Text("Benvenuto");
@@ -121,8 +171,46 @@ public class StartJavaFX extends Application {
 
             Label lblUsername = new Label("Username:");
             final TextField txtUsername = new TextField();
-            Label lblColor = new Label("Colore:");
-            final TextField txtColor = new TextField();
+            Label lblColor = new Label("Scegli un colore:");
+
+            Image imgY;
+            Button buttonY = new Button();
+            Image imgR;
+            Button buttonR = new Button();
+            Image imgB;
+            Button buttonB = new Button();
+            Image imgG;
+            Button buttonG = new Button();
+            Image imgGr;
+            Button buttonGr = new Button();
+            try {
+                imgY = new Image(new FileInputStream("src/main/resources/images/yellow.png"),100,100,true,true);
+                buttonY.setGraphic(new ImageView(imgY));
+
+                imgR = new Image(new FileInputStream("src/main/resources/images/red.png"),100,100,true,true);
+                buttonR.setGraphic(new ImageView(imgR));
+
+                imgB = new Image(new FileInputStream("src/main/resources/images/blue.png"),100,100,true,true);
+                buttonB.setGraphic(new ImageView(imgB));
+
+                imgG = new Image(new FileInputStream("src/main/resources/images/green.png"),100,100,true,true);
+                buttonG.setGraphic(new ImageView(imgG));
+
+                imgGr = new Image(new FileInputStream("src/main/resources/images/gray.png"),100,100,true,true);
+                buttonGr.setGraphic(new ImageView(imgGr));
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            HBox colorList = new HBox(10);
+            colorList.getChildren().add(buttonY);
+            colorList.getChildren().add(buttonB);
+            colorList.getChildren().add(buttonR);
+            colorList.getChildren().add(buttonG);
+            colorList.getChildren().add(buttonGr);
+
+
             Button btnLogin = new Button("Login");
             HBox hbBtn = new HBox(10);
             hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
@@ -132,11 +220,17 @@ public class StartJavaFX extends Application {
             loginGrid.add(lblUsername,0,2);
             loginGrid.add(txtUsername,0,3);
             loginGrid.add(lblColor,0,4);
-            loginGrid.add(txtColor,0,5);
+            loginGrid.add(colorList,0,5);
             loginGrid.add(btnLogin,0,6);
 
             final Text actiontarget = new Text();
             loginGrid.add(actiontarget, 0, 8);
+
+            buttonY.setOnAction(e-> color = "yellow");
+            buttonR.setOnAction(e-> color = "red");
+            buttonB.setOnAction(e-> color = "blue");
+            buttonG.setOnAction(e-> color = "green");
+            buttonGr.setOnAction(e-> color = "gray");
 
             btnLogin.setOnAction(e -> {
 
@@ -144,7 +238,6 @@ public class StartJavaFX extends Application {
                 actiontarget.setText("Accesso...");
 
                 username = txtUsername.getText();
-                color = txtColor.getText();
 
             });
 
