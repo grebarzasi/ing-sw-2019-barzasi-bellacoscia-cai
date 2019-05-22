55040de93e6fdb382f07d64fe902b3cca19c028a
theodoretsai <yuting.cai@mail.polimi.it>
2019-05-23 01:17:41 +0200

    Modified Controller Skeleton

--- src/main/java/it/polimi/ingsw/Controller/asBot.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.Controller;
-
-public class asBot implements ControllerState{
-
-}
--- src/main/java/it/polimi/ingsw/Controller/asSelf.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.Controller;
-
-public class asSelf implements ControllerState{
-}
