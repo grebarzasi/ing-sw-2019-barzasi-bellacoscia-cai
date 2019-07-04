abe862cc3d06f995e28a014c4b6b19a59b93fcd3
grebarzasi <48253379+grebarzasi@users.noreply.github.com>
2019-07-04 15:09:22 +0200

    sh

---
+++ out/artifacts/client/client - Copia.sh
@@ -0,0 +1,2 @@
+java -jar -Dfile.encoding=UTF8 ing-sw-2019-barzasi-bellacoscia-cai.jar
+PAUSE
---
+++ out/artifacts/server/server - Copia.sh
@@ -0,0 +1,4 @@
+echo $LANG
+# en_GB.UTF-8
+java -jar -Dfile.encoding=windows-1252 ing-sw-2019-barzasi-bellacoscia-cai.jar
+PAUSE
